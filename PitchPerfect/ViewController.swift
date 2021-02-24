cde5064c95dea786125b4fc2a9e0f560dcba4da5
MoonkiKim(김문기) <rlaansrl789@gmail.com>
2021-02-25 01:13:41 +0900

    feature : record view controller

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLable: UILabel!
+    
+    @IBOutlet weak var recordingButton: UIButton!
+    
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        stopRecordingButton.isEnabled = false
     }
 
 
+    @IBAction func recordAudio(_ sender: Any) {
+        recordingLable.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordingButton.isEnabled = false
+    }
+    
+    @IBAction func stopRecording(_ sender: Any) {
+        recordingLable.text = "Tap to Record"
+        stopRecordingButton.isEnabled = false
+        recordingButton.isEnabled = true
+    }
 }
 
